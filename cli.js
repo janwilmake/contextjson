74e1d7749fec66c3ae13f22005ff94d0c9a47ecd
Jan Wilmake <jan@wilmake.com>
2025-05-09 09:17:27 +0200

    improvements

--- cli.js
+++ cli.js
@@ -1,3 +1,14 @@
 #!/usr/bin/env node
+const fs = require("fs");
 
-console.log("Hello, world!");
+const cli = async () => {
+  const exists = fs.existsSync("context.json");
+  if (!exists) {
+    console.log("context.json not found");
+    return;
+  }
+
+  const context = fs.readFileSync("context.json");
+  console.log("context.json found. length: ", context.length);
+};
+cli();
